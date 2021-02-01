13a7cb1827d15ddc0484350a50beda0fd748a744
Scott Wilbur <swilbur@refracta>
2021-02-01 14:29:03 +0000

    clean up detector logic, allow curved fibers

--- src/FlexRayTrackingAction.cc
+++ src/FlexRayTrackingAction.cc
@@ -21,28 +21,44 @@ void FlexRayTrackingAction::PostUserTrackingAction(const G4Track* track)
   // get final position of track
   G4ThreeVector pos = track->GetPosition();
   
-  // this should probably be done with G4SensitiveDetectors instead of raw position once we know more about what we're simulating
-  if(fabs(pos.x()) < geo::fiberLength*0.45 && fabs(pos.y()) < geo::fiberLength*0.45) return;
-  
-  G4String vol = track->GetVolume()->GetName();
-  if(vol != "Core" && vol != "InnerCladding" && vol != "OuterCladding") return;
+  // check that it ended at the end of a fiber
+  if(fabs(pos.y()) < geo::fiberLength * 0.5 * 0.99){
+    if(geo::bendTheta < 0.01 * deg && fabs(pos.x()) < geo::fiberLength * 0.5 * 0.99) return;
+    if(geo::bendTheta > 0.01 * deg && fabs(atan(pos.x()/(pos.z()+geo::bendRadius))) < geo::bendTheta * 0.5 * 0.99) return;
+  }
 
-  int detectorIndex=0;
+  const G4VTouchable *touch = track->GetTouchable();
+  G4VPhysicalVolume *vol = touch->GetVolume();
+  G4String name = touch->GetVolume()->GetName();
+  
+  if(name == "Core"){
+    vol = touch->GetVolume(2);
+  }else if(name == "InnerCladding"){
+    vol = touch->GetVolume(1);
+  }else if(name == "OuterCladdingX" || name == "OuterCladdingY"){
+    //vol = vol;
+  }else{
+    return; // not in a fiber
+  }
+
+  name = vol->GetName();
+  int detectorIndex = vol->GetCopyNo();
 
   double internX=0, internY=0; // internal x and y inside the fiber, to tell which SPAD was hit
 
-  if(fabs(pos.y()) > geo::fiberLength*0.45){ // it's an x measurement
-    detectorIndex = pos.x()/geo::fiberSpacing + geo::numFibers*0.5;
-
-    internX = pos.x() - (detectorIndex - (geo::numFibers-1)*0.5) * geo::fiberSpacing;
-    internY = pos.z() - geo::layerSpacing*0.5;
+  if(name == "OuterCladdingX"){ // it's an x measurement
+    internX = pos.x() - vol->GetTranslation().x();
+    internY = pos.z() - vol->GetTranslation().z();
 
     if(pos.y() > 0) detectorIndex |= (1<<8);
   }else{ // it's a y measurement
-    detectorIndex = pos.y()/geo::fiberSpacing + geo::numFibers*0.5;
+    internX = pos.y() - vol->GetTranslation().y();
+    internY = pos.z() - vol->GetTranslation().z();
 
-    internX = pos.y() - (detectorIndex - (geo::numFibers-1)*0.5) * geo::fiberSpacing;
-    internY = pos.z() + geo::layerSpacing*0.5;
+    if(geo::bendTheta > 0.01 * deg){
+      if(pos.x() > 0) internY = (pos.z()+geo::bendRadius)*cos(geo::bendTheta/2) + pos.x()*sin(geo::bendTheta/2) - geo::bendRadiusY;
+      if(pos.x() < 0) internY = (pos.z()+geo::bendRadius)*cos(geo::bendTheta/2) - pos.x()*sin(geo::bendTheta/2) - geo::bendRadiusY;
+    }
 
     detectorIndex |= (1<<9);
     if(pos.x() > 0) detectorIndex |= (1<<8);
